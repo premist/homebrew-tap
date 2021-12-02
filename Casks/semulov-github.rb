5163d77b09f384767e3b0c7b2f17a58e3567124a
Minku Lee <public@premi.st>
2021-12-03 00:00:05 +0900

    Add semulov-github

---
+++ Casks/semulov-github.rb
@@ -0,0 +1,11 @@
+cask "semulov-github" do
+  version "2.6"
+  sha256 "fc25b015251562df79c89143b2fb123fc793d780e3f1eb9990d6d5723c870dc6"
+
+  url "https://github.com/kainjow/Semulov/releases/download/v2.6/Semulov.zip"
+  name "Semulov GitHub"
+  desc "Access mounted and unmounted volumes from the menubar"
+  homepage "https://github.com/kainjow/Semulov"
+
+  app "Semulov.app"
+end
